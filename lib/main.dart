0dbf2a46ca8c7d3ec6686038f1592cfd76160732
kaushikkachhadiya <kachhadiya.kaushik110@gmail.com>
2023-06-08 16:01:54 +0530

    bookstore_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookstore/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimationDemo(),
+      home: SplashScreen(),
     );
   }
 }
